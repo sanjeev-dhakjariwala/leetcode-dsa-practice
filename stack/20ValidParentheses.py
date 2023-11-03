bf32439b54682d4c6cff7dd0e35516b06c37f272
Sanjeev Jha <sanjeevjha882@gmail.com>
2023-11-03 15:57:06 +0530

    ValidParenthesis Code Added

---
+++ stack/20ValidParentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stk = []
+
+        for i in s:
+            if i == '(' or i == '[' or i == '{':
+                stk.append(i)
+            else:
+                if len(stk) > 0:
+                    ele = stk.pop()
+                    if ((ele == '(' and i == ')') or (ele == '[' and i == ']') or (ele == '{' and i == '}') ):
+                        continue
+                    else:
+                        return False
+                else:
+                    return False
+        return len(stk) == 0
+
+sol = Solution()
+print(sol.isValid("(){}}{"))
