f590192954243ed8b90dc617b473f4e261a1fe28
Sanjeev Jha <sanjeevjha882@gmail.com>
2023-11-05 01:19:00 +0530

    ListNode and LinkedListCycle Code added

---
+++ linked_list/141LinkedListCycle.py
@@ -0,0 +1,12 @@
+from ListNode import *
+from typing import *
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        fast, slow = head, head
+        while fast != None and fast.next != None:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                return True
+
+        return False 
