3074abbc2ddb542382ce8df1230b44d138993f20
Sanjeev Jha <sanjeevjha882@gmail.com>
2023-11-04 11:39:24 +0530

    memo added to MinCostClimbingStairs

--- dynamic_programming/746MinCostClimbingStairs.py
+++ dynamic_programming/746MinCostClimbingStairs.py
@@ -6,10 +6,39 @@ class Solution:
         def helper(n):
             if n <= 1:
                 return cost[n]
-            return cost[n] + min(helper(n - 1), helper(n - 2))
+            option1 = helper(n - 1)
+            option2 = helper(n - 2)
+            return cost[n] + min(option1, option2)
 
         n = len(cost)
-        return min(helper(n - 1), helper(n - 2))
+        
+
+        memo = [-1] * (len(cost) + 1)
+        def helper(i):
+            if i <= 1:
+                return cost[i]
+            if memo[i] != -1:
+                return memo[i]
+            option1 = helper(i - 1)
+            option2 = helper(i - 2)
+            memo[i] = cost[i] + min(option1, option2)
+            return memo[i]
+        n = len(cost) - 1
+        option1 = helper(n - 1)
+        option2 = helper(n - 2)
+        return min(option1, option2)
+        return min(helper(n), helper(n - 1))
+"""     def minCostClimbingStairs(self, cost: List[int]) -> int:
+        def helper(cost, i):
+            if i >= len(cost):
+                return 0
+            option1 = helper(cost, i + 1)
+            option2 = helper(cost, i + 2)
+            return cost[i] + min(option1, option2)
+
+        choice1 = helper(cost, 0)
+        choice2 = helper(cost, 1)
+        return min(choice1, choice2) """
 
 
 sol = Solution()
