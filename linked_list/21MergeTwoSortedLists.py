e0c1cedaad3e5414791d88605638c4f458281370
Sanjeev Jha <sanjeevjha882@gmail.com>
2023-11-15 00:53:53 +0530

    Code added to MergeTwoSortedList to run it locally

--- linked_list/21MergeTwoSortedLists.py
+++ linked_list/21MergeTwoSortedLists.py
@@ -5,9 +5,10 @@
 #         self.next = next
 from typing import *
 from linked_list.ListNode import *
+
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
+        dummy = ListNode(-1)
         temp = dummy
 
         while list1 and list2:
@@ -20,3 +21,12 @@ class Solution:
             temp = temp.next
         temp.next = list1 or list2
         return dummy.next
+
+
+list1 = create_linked_list_from_string("1,2,4")
+list2 = create_linked_list_from_string("1,3,4")
+sol = Solution()
+current = sol.mergeTwoLists(list1, list2)
+while current:
+    print(current.val, end=" -> ")
+    current = current.next
--- linked_list/ListNode.py
+++ linked_list/ListNode.py
@@ -3,6 +3,9 @@ class ListNode:
         self.val = x
         self.next = None
 
+
+
+
 def create_linked_list_from_string(input_string):
     # Split the input_string by commas to get individual values
     values = input_string.split(',')
@@ -19,6 +22,7 @@ def create_linked_list_from_string(input_string):
 
     # Return the linked list, starting from the actual head (not the initial None value)
     return head.next
+
 
 # Example usage:
 input_string = "3,2,0,-4"
