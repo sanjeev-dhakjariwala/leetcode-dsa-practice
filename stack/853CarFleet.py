3b86d5376a6996b4408f986aad2c8c37f2594182
Sanjeev Jha <sanjeevjha882@gmail.com>
2023-11-18 19:03:08 +0530

    CarFleet Code Added

---
+++ stack/853CarFleet.py
@@ -0,0 +1,13 @@
+from typing import *
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pair = [(p, s) for p, s in zip(position, speed)]
+        pair.sort(reverse=True)
+        stack = []
+        for p, s in pair:  # Reverse Sorted Order
+            stack.append((target - p) / s)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
+sol = Solution()
+print(sol.carFleet(12, [10, 8, 0, 5, 3], [2, 4, 1, 1, 3]))
