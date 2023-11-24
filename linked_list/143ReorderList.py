54af617d55d3dc27c7167ed2caba679024b8930f
Sanjeev Jha <sanjeevjha882@gmail.com>
2023-11-24 16:28:37 +0530

    ReorderList Code Added

--- linked_list/143ReorderList.py
+++ linked_list/143ReorderList.py
@@ -5,8 +5,29 @@
 #         self.next = next
 from typing import *
 from linked_list.ListNode import *
+
+
 class Solution:
-    def reorderList(self, head: Optional[ListNode]) -> None:
-        """
-        Do not return anything, modify head in-place instead.
-        """
+    def reorderList(self, head: ListNode) -> None:
+        # find middle
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # reverse second half
+        second = slow.next
+        prev = slow.next = None
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+
+        # merge two halfs
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
