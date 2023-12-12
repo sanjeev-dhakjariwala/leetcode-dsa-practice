91c818d48ec46e482d51b1ccfd4400bf9a45730f
Sanjeev Jha <sanjeevjha882@gmail.com>
2023-12-12 19:17:22 +0530

    12-12-2023

---
+++ array_and_hashing/41FirstMissingPositive.py
@@ -0,0 +1,22 @@
+from typing import *
+
+
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        # Swap elements to their correct positions
+        for i in range(n):
+            while 1 <= nums[i] <= n and nums[nums[i] - 1] != nums[i]:
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+
+        # Find the first missing positive integer
+        for i in range(n):
+            if nums[i] != i + 1:
+                return i + 1
+
+        return n + 1
+
+
+sol = Solution()
+print(sol.firstMissingPositive([3, 4, -1, 1]))
