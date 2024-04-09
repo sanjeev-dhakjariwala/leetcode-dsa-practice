a03b613dea9b88382fdba755e21e7d86e0186f78
sanjeev-dhakjariwala <sanjeevjha882@gmail.com>
2024-04-10 01:43:31 +0530

    Leetcode 746

--- dynamic_programming/746MinCostClimbingStairs.py
+++ dynamic_programming/746MinCostClimbingStairs.py
@@ -3,42 +3,31 @@ from typing import *
 
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        def helper(n):
-            if n <= 1:
-                return cost[n]
-            option1 = helper(n - 1)
-            option2 = helper(n - 2)
-            return cost[n] + min(option1, option2)
 
-        n = len(cost)
-        
-        # memoization code added
-        """ memo = [-1] * (len(cost) + 1)
-        def helper(i):
-            if i <= 1:
-                return cost[i]
-            if memo[i] != -1:
-                return memo[i]
-            option1 = helper(i - 1)
-            option2 = helper(i - 2)
-            memo[i] = cost[i] + min(option1, option2)
-            return memo[i] """
-        option1 = helper(n - 1)
-        option2 = helper(n - 2)
-        return min(option1, option2)
-        n = len(cost) - 1
-        return min(helper(n), helper(n - 1))
-"""     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        def helper(cost, i):
+        def helperMemo(i):
+            if i in dp:
+                return dp[i]
             if i >= len(cost):
                 return 0
-            option1 = helper(cost, i + 1)
-            option2 = helper(cost, i + 2)
-            return cost[i] + min(option1, option2)
+            cost1 = helperMemo(i + 1)
+            cost2 = helperMemo(i + 2)
+            dp[i] = cost[i] + min(cost1, cost2)
+            return cost[i] + min(cost1, cost2)
 
-        choice1 = helper(cost, 0)
-        choice2 = helper(cost, 1)
-        return min(choice1, choice2) """
+        dp = {}
+        # return min(helperMemo(0), helperMemo(1))
+
+        def helper(i):
+            if i == (len(cost) - 1):
+                return cost[i]
+            if i >= len(cost):
+                return 0
+            cost1 = helper(i + 1)
+            cost2 = helper(i + 2)
+            finalCost = cost[i] + min(cost1, cost2)
+            return finalCost
+
+        return min(helper(0), helper(1))
 
 
 sol = Solution()
