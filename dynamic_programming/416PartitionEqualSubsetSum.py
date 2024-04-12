5638f00c82d98059e52ea8669fc768872d73dd5a
sanjeev-dhakjariwala <sanjeevjha882@gmail.com>
2024-04-13 00:08:01 +0530

    13-APR-2024

--- dynamic_programming/416PartitionEqualSubsetSum.py
+++ dynamic_programming/416PartitionEqualSubsetSum.py
@@ -1,24 +1,41 @@
+from typing import *
+
+
 class Solution:
-    def canPartition(self, nums):
-        total_sum = sum(nums)
-        
-        # If the total sum is odd, it can't be divided into two equal subsets.
-        if total_sum % 2 != 0:
-            return False
-        
-        half_sum = total_sum // 2
-        
-        # Initialize a 1D array to store whether a sum 'i' can be achieved with the elements in 'nums'.
-        dp = [False] * (half_sum + 1)
-        print(dp)
-        dp[0] = True
-        
-        # Iterate through the elements in 'nums' and update the 'dp' array.
-        for num in nums:
-            for i in range(half_sum, num - 1, -1):
-                dp[i] = dp[i] or dp[i - num]
-        
-        return dp[half_sum]
+
+    def canPartition(self, nums: List[int]) -> bool:
+        n = len(nums)
+        dp = {}
+
+        def helperMemo(i, rem):
+            if (i, n) in dp:
+                return dp[(i, n)]
+            if i == n - 1 and rem == 0:
+                return True
+            if i >= n or rem < 0:
+                return False
+
+            include = helperMemo(i + 1, rem - nums[i])
+            exclude = helperMemo(i + 1, rem)
+            dp[(i, rem)] = include or exclude
+            return include or exclude
+
+        def helper1DDP():
+            total_sum = sum(nums)
+            if total_sum % 2 != 0:
+                return False
+
+            target_sum = total_sum // 2
+            dp = [False] * (target_sum + 1)
+            dp[0] = True
+
+            for num in nums:
+                for j in range(target_sum, num - 1, -1):
+                    dp[j] = dp[j] or dp[j - num]
+
+            return dp[target_sum]
+        return helper1DDP()
+
 
 sol = Solution()
-print(sol.canPartition([1,5,11,5]))
+print(sol.canPartition([1, 5, 11, 5]))
