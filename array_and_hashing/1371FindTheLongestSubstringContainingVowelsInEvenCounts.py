36333d4b7424ddd80fb5303275aafffaa3805424
sanjeev-dhakjariwala <sanjeevjha882@gmail.com>
2024-05-06 10:23:39 +0530

    06-May-2024

--- array_and_hashing/1371FindTheLongestSubstringContainingVowelsInEvenCounts.py
+++ array_and_hashing/1371FindTheLongestSubstringContainingVowelsInEvenCounts.py
@@ -9,7 +9,7 @@ class Solution:
         for i, char in enumerate(s):
             if char in vowels:
                 # Toggle the corresponding bit in XOR if the vowel is encountered
-                xor ^= 1 << (ord(char) - ord("a"))
+                xor ^= (ord(char) - ord("a") + 1)
 
             # Check if the current XOR value has been encountered before
             if xor in xor_to_index:
@@ -18,7 +18,46 @@ class Solution:
                 xor_to_index[xor] = i
 
         return max_length
+        # c_m = [
+        #     1,
+        #     0,
+        #     0,
+        #     0,
+        #     2,
+        #     0,
+        #     0,
+        #     0,
+        #     4,
+        #     0,
+        #     0,
+        #     0,
+        #     0,
+        #     0,
+        #     8,
+        #     0,
+        #     0,
+        #     0,
+        #     0,
+        #     0,
+        #     16,
+        #     0,
+        #     0,
+        #     0,
+        #     0,
+        #     0,
+        # ]
+        # mask = 0
+        # res = 0
+        # m = [-1] * 32
+
+        # for i in range(len(s)):
+        #     mask ^= c_m[ord(s[i]) - ord("a")]
+        #     if mask != 0 and m[mask] == -1:
+        #         m[mask] = i
+        #     res = max(res, i - m[mask])
+
+        # return res
 
 
 sol = Solution()
-print(sol.findTheLongestSubstring("ixzhsdka"))
+print(sol.findTheLongestSubstring("eleetminicoworoep"))
